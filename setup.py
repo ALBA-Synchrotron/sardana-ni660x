305e72e3cf676d51575f3f55c403a93e2d8e2896
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-01-13 10:34:07 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+from setuptools import setup, find_packages
+
+
+def main():
+    """Main method collecting all the parameters to setup."""
+    name = "sardana-ni660x"
+    version = "0.1.0"
+    description = "NI660X Sardana Controller"
+    author = "ALBA controls team"
+    author_email = "controls@cells.es"
+    license = "GPLv3"
+    url = "http://github.com/ALBA-Synchrotron/sardana-ni660x"
+    packages = find_packages()
+
+    # Add your dependencies in the following line.
+    install_requires = ['sardana']
+
+    python_requires = '>=3.5'
+
+    setup(
+        name=name,
+        version=version,
+        description=description,
+        author=author,
+        author_email=author_email,
+        license=license,
+        url=url,
+        packages=packages,
+        install_requires=install_requires,
+        python_requires=python_requires,
+    )
+
+if __name__ == "__main__":
+    main()
